4871efed42a882876223936f36dc9b35d6546ffa
hawk0489 <hwk7520@gmail.com>
2024-07-15 16:21:08 +0800

    Add files via upload

---
+++ Models/PetDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace PetInfo.Models
+{
+    public class PetDbContext : DbContext
+    {
+        public PetDbContext(DbContextOptions<PetDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Pet> Pets { get; set; }
+    }
+}
